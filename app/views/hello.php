dc7232cfb741a10ace0250973ae5b6c824053586
Mike Daly <mtkdaly@hotmail.com>
2014-09-25 13:54:23 +0100

    Initial Commit

---
+++ app/views/hello.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	
+</head>
+<body>
+	
+	<h1>GetStream.io test</h1>
+	
+	
+		
+</body>
+</html>
